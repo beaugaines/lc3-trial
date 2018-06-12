f0e8a6bf83eacbc4d5adaf92122a2a75d1db8cfc
Charlie Gaines <beaugaines@gmail.com>
2018-06-12 09:34:37 -0400

    protect from record not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def index
   end
+
+  private
+  def record_not_found
+    flash[:alert] = 'That record was not found in our system'
+    redirect_to(request.referrer || root_path)
+  end
+
 end
